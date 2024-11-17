417a7413f64ea8c0edc36bf8ee3b2261f056e9d3
Maria José Landa Fernández <maryjoland24@gmail.com>
2024-11-16 19:37:05 -0600

    Create majo240499.cpp

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/c++/majo240499.cpp
@@ -0,0 +1,28 @@
+// sitio web oficial https://isocpp.org/
+// comentario en una linea
+
+/*comentario
+en varias
+líneas*/
+
+#inlcude <iostream>
+using namespace std;
+
+int main(){
+  //variable
+int numero;
+
+//constante
+const float = 3.1416;
+
+//variables
+int enteros = 2;
+float decimales = 2.76;
+string cadenas = "Hola mundo";
+char caracter =  'c';
+doble numdecimal = 5.67895;
+bool miPrimerPrograma = true;
+
+cout<<"Hola, C++"<<string;
+}
+
